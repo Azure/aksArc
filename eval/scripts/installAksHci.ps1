6af7a1d39fccf56c8b0886c86aaf95abb55269a5
Matt McSpirit <mattmc@outlook.com>
2021-09-16 15:48:41 -0700

    Testing

--- eval/scripts/installAksHci.ps1
+++ eval/scripts/installAksHci.ps1
@@ -1,4 +1,147 @@
-$ProgressPreference = "SilentlyContinue"
+[CmdletBinding()]
+param 
+(
+    [Parameter(Mandatory)]
+    [string]$rgName,
+    [Parameter(Mandatory)]
+    [string]$location,
+    [Parameter(Mandatory)]
+    [string]$subId,
+    [Parameter(Mandatory)]
+    [string]$tenantId,
+    [Parameter(Mandatory)]
+    [string]$domainName,
+    [Parameter(Mandatory)]
+    [SecureString]$adminUsername,
+    [Parameter(Mandatory)]
+    [SecureString]$adminPassword,
+    [Parameter(Mandatory)]
+    [SecureString]$appId,
+    [Parameter(Mandatory)]
+    [SecureString]$appSecret,
+    [Parameter(Mandatory)]
+    [string]$installWAC,
+    [Parameter(Mandatory)]
+    [string]$aksHciNetworking,
+    [Parameter(Mandatory)]
+    [string]$kubernetesVersion,
+    [Parameter(Mandatory)]
+    [int]$controlPlaneNodes,
+    [Parameter(Mandatory)]
+    [string]$controlPlaneNodeSize,
+    [Parameter(Mandatory)]
+    [string]$loadBalancerSize,
+    [Parameter(Mandatory)]
+    [int]$linuxWorkerNodes,
+    [Parameter(Mandatory)]
+    [string]$linuxWorkerNodeSize,
+    [Parameter(Mandatory)]
+    [int]$windowsWorkerNodes,
+    [Parameter(Mandatory)]
+    [string]$windowsWorkerNodeSize
+)
+function Log($out) {
+    $out = [System.DateTime]::Now.ToString("yyyy.MM.dd hh:mm:ss") + " ---- " + $out;
+    Write-Output $out;
+}
 
-mkdir -Path "C:\TestAksHci"
-New-item -Path 'C:\TestAksHci' -Name "TestAksHciScript.txt" -ItemType File -Force -Verbose
+function DecodeParam($parameter) {
+    if ($parameter.StartsWith("base64:")) {
+        $encodedParameter = $parameter.Split(':', 2)[1]
+        $decodedArray = [System.Convert]::FromBase64String($encodedParameter);
+        $parameter = [System.Text.Encoding]::UTF8.GetString($decodedArray); 
+    }
+    return $parameter
+}
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+$Global:VerbosePreference = "Continue"
+$Global:ErrorActionPreference = 'Stop'
+$Global:ProgressPreference = 'SilentlyContinue'
+
+$ScriptLocation = Get-Location
+
+### SET LOG LOCATION ###
+$logPath = "$($env:SystemDrive)\InstallAksHciLog"
+
+if (![System.IO.Directory]::Exists("$logPath")) {
+    New-Item -Path $logPath -ItemType Directory -Force -ErrorAction Stop
+}
+
+### CONFIG POWER OPTIONS ###
+Log "Configure Power Options to High performance mode."
+POWERCFG.EXE /S SCHEME_MIN
+
+### START LOGGING ###
+$runTime = $(Get-Date).ToString("MMdd-HHmmss")
+$fullLogPath = "$logPath\InstallAksHci$runTime.txt"
+Start-Transcript -Path "$fullLogPath" -Append
+Log "Creating log folder"
+Log "Log folder has been created at $logPath"
+Log "Log file stored at $fullLogPath"
+Log "Starting logging"
+Log "Log started at $runTime"
+
+### DECODING PARAMETERS
+Log "Decoding secure parameters passed from ARM template"
+$adminUsername = DecodeParam $adminUsername
+$adminPassword = DecodeParam $adminPassword
+$appId = DecodeParam $appId
+$appSecret = DecodeParam $appSecret
+
+### CREATE STRONG PASSWORDS ###
+Log "Configuring strong passwords for the user accounts"
+$strAdminPassword = ConvertTo-SecureString $adminPassword -Force -AsPlainText -Verbose
+$strAppSecret = ConvertTo-SecureString $appSecret -Force -AsPlainText -Verbose
+
+### CREATE CREDENTIALS ###
+Log "Configuring credential objects"
+[System.Management.Automation.PSCredential]$domainCreds = New-Object System.Management.Automation.PSCredential ("${domainName}\$($adminUsername)", $strAdminPassword)
+[System.Management.Automation.PSCredential]$nodeLocalCreds = New-Object System.Management.Automation.PSCredential ($adminUsername, $adminPassword)
+[System.Management.Automation.PSCredential]$spCreds = New-Object System.Management.Automation.PSCredential ($appId, $strAppSecret)
+
+$targetDrive = "V"
+$targetAksPath = "$targetDrive" + ":\AKS-HCI"
+$loadBalancerSize = ($loadBalancerSize).Split(" ", 2)[0]
+$controlPlaneNodeSize = ($controlPlaneNodeSize).Split(" ", 2)[0]
+$linuxWorkerNodeSize = ($linuxWorkerNodeSize).Split(" ", 2)[0]
+$windowsWorkerNodeSize = ($windowsWorkerNodeSize).Split(" ", 2)[0]
+
+<# Initialize AKS-HCI
+try {
+    $initialized = Test-Path -Path "C:\AksHciAutoDeploy\InitializeAksHci.txt"
+    if (!$initialized) {
+        Log "Node has not been previously initialized - initializing now"
+        Initialize-AksHciNode
+        New-item -Path C:\AksHciAutoDeploy\ -Name "InitializeAksHci.txt" -ItemType File -Force -Verbose
+        Log "Initialization completed"
+    }
+    else {
+        Log "Node has been previously initialized - Moving to next step"
+    }
+}
+catch {
+    Log "Something went wrong with running Initialize-AksHci. Please review the log file at $fullLogPath and redeploy your VM."
+    Set-Location $ScriptLocation
+    throw $_.Exception.Message
+    return
+} #>
+
+Log "Resource group = $rgName"
+Log "Location = $location"
+Log "SubID = $subId"
+Log "tenantID = $tenantId"
+Log "Domain Name = $domainName"
+Log "Admin User = $adminUsername"
+Log "App ID = $appId"
+Log "Install WAC = $installWAC"
+Log "Networking config = $aksHciNetworking"
+Log "Kubernetes Version = $kubernetesVersion"
+Log "Number of Control Plane Nodes = $controlPlaneNodes of size: $controlPlaneNodeSize"
+Log "Number of Linux Nodes = $linuxWorkerNodes of size: $linuxWorkerNodeSize"
+Log "Number of Windows Plane Nodes = $windowsWorkerNodes of size: $windowsWorkerNodeSize"
+Log "LB Size = $loadBalancerSize"
+
+$endTime = $(Get-Date).ToString("MMdd-HHmmss")
+Log "Logging stopped at $endTime"
+Stop-Transcript -ErrorAction SilentlyContinue
