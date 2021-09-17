71d88c2fe6e7184dcf8c99d3a6382c8a22fc5c8c
Matt McSpirit <mattmc@outlook.com>
2021-09-16 18:34:50 -0700

    Test

--- eval/scripts/installAksHci.ps1
+++ eval/scripts/installAksHci.ps1
@@ -106,11 +106,11 @@ Log "Domain Name = $domainName"
 Log "Admin User = $adminUsername"
 Log "App ID = $appId"
 Log "Creating domain creds"
-[System.Management.Automation.PSCredential]$domainCreds = New-Object System.Management.Automation.PSCredential ("${domainName}\$($adminUsername)", $strAdminPassword)
+$domainCreds = New-Object System.Management.Automation.PSCredential ("$domainName\$adminUsername", $strAdminPassword)
 Log "Creating node local creds"
-[System.Management.Automation.PSCredential]$nodeLocalCreds = New-Object System.Management.Automation.PSCredential ($adminUsername, $adminPassword)
+$nodeLocalCreds = New-Object System.Management.Automation.PSCredential ($adminUsername, $adminPassword)
 Log "Creating SP creds"
-[System.Management.Automation.PSCredential]$spCreds = New-Object System.Management.Automation.PSCredential ($appId, $strAppSecret)
+$spCreds = New-Object System.Management.Automation.PSCredential ($appId, $strAppSecret)
 
 $targetDrive = "V"
 $targetAksPath = "$targetDrive" + ":\AKS-HCI"
