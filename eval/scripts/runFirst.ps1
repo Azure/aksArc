596337166ec8e101499b70a2c2f196c624a6f5d6
Matt McSpirit <mattmc@outlook.com>
2021-09-17 21:27:48 -0700

    Cleanup

--- eval/scripts/runFirst.ps1
+++
@@ -1,8 +0,0 @@
-
-
-
-$password =  ConvertTo-SecureString "[your admin account user password]" -AsPlainText -Force
-$credential = New-Object System.Management.Automation.PSCredential("$env:COMPUTERNAME\[your admin account]", $password)
-$command = $file = $PSScriptRoot + "\installAksHci.ps1"
-Enable-PSRemoting –force
-Invoke-Command -FilePath $command -Credential $credential -ComputerName $env:COMPUTERNAME
