e17d89061e03f32d5919c34409af3e34144f11f7
Matt McSpirit <mattmc@outlook.com>
2021-09-17 19:55:37 -0700

    Fixes

--- eval/scripts/removeCustomScript.ps1
+++ eval/scripts/removeCustomScript.ps1
@@ -16,7 +16,17 @@ param
 )
 
 $ErrorActionPreference = 'Stop'
+function DecodeParam($parameter) {
+    if ($parameter.StartsWith("base64:")) {
+        $encodedParameter = $parameter.Split(':', 2)[1]
+        $decodedArray = [System.Convert]::FromBase64String($encodedParameter);
+        $parameter = [System.Text.Encoding]::UTF8.GetString($decodedArray); 
+    }
+    return $parameter
+}
+
+$appSecret = DecodeParam $appSecret
 $strAppSecret = ConvertTo-SecureString $appSecret -Force -AsPlainText -Verbose
 $spCreds = New-Object System.Management.Automation.PSCredential ($appId, $strAppSecret)
 Connect-AzAccount -Credential $spCreds -ServicePrincipal -Tenant $tenantId -Subscription $subscriptionId
-Remove-AzVMCustomScriptExtension -Name 'InstallWindowsAdminCenter' -ResourceGroupName $resourceGroupName -VMName $vmName
+Remove-AzVMCustomScriptExtension -Name 'InstallWindowsAdminCenter' -ResourceGroupName $resourceGroupName -VMName $vmName -Force
