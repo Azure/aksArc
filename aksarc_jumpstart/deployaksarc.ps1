80abc092de1026873349d9895e68aa8b62ff177b
Madhan Raj Mookkandy <madhanm@microsoft.com>
2025-11-07 04:40:46 +0000

    Add Param for cl creation

--- aksarc_jumpstart/deployaksarc.ps1
+++ aksarc_jumpstart/deployaksarc.ps1
@@ -54,7 +54,7 @@ $scriptToExecute = [ordered] @{
   "$scriptLocation/deployappliance.ps1" = "deployappliance.ps1 -resource_group ""$GroupName"" -appliance_name ""$applianceName"" -workDirectory ""$workingDir"" -location ""$Location"" -subscription ""$subscription"" ";
   "$scriptLocation/deployaksarcextension.ps1" = "deployaksarcextension.ps1 -resource_group ""$GroupName"" -appliance_name ""$applianceName"" -workDirectory ""$workingDir"" -location ""$Location"" -subscription ""$subscription""";
   "$scriptLocation/deployvmssextension.ps1" = "deployvmssextension.ps1 -resource_group ""$GroupName"" -appliance_name ""$applianceName"" -workDirectory ""$workingDir"" -location ""$Location"" -subscription ""$subscription""";
-  "$scriptLocation/deploycustomlocation.ps1" = "deploycustomlocation.ps1 -resource_group ""$GroupName"" -appliance_name ""$applianceName"" -workDirectory ""$workingDir"" -location ""$Location"" -subscription ""$subscription""";
+  "$scriptLocation/deploycustomlocation.ps1" = "deploycustomlocation.ps1 -resource_group ""$GroupName"" -appliance_name ""$applianceName"" -customLocationName ""$customLocationName"" -workDirectory ""$workingDir"" -location ""$Location"" -subscription ""$subscription""";
   "$scriptLocation/deploylnet.ps1" = "deploylnet.ps1 -resource_group ""$GroupName""  -lnetName ""$ArcLnetName"" -customLocationName ""$customLocationName"" -location ""$Location"" -subscription ""$subscription""";
   "$scriptLocation/deployaksarccluster.ps1" = "deployaksarccluster.ps1 -resource_group ""$GroupName"" -lnetName ""$ArcLnetName"" -customLocationName ""$customLocationName"" -subscription ""$subscription""";
 }
