50f5275ea79480f2261f4b3be97f525303517609
Srikant Sarwa <srikantsarwa@microsoft.com>
2025-11-10 14:38:37 -0800

    fix azure rbac on the aks cluster

--- aksarc_jumpstart/scripts/deployaksarccluster.ps1
+++ aksarc_jumpstart/scripts/deployaksarccluster.ps1
@@ -14,6 +14,6 @@ az account set -s $subscription
 $clId = az customlocation show --name $customLocationName --resource-group $resource_group --query "id" -o tsv
 
 $lnetId = az stack-hci-vm network lnet show --name $lnetName -g $resource_group --query id -o tsv
-az aksarc create --name $aksArcClusterName --resource-group $resource_group --custom-location $clId --vnet-ids $lnetId  --generate-ssh-keys
+az aksarc create --name $aksArcClusterName --resource-group $resource_group --custom-location $clId --vnet-ids $lnetId --enable-azure-rbac --generate-ssh-keys
 
 Stop-Transcript
